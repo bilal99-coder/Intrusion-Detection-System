f28accb78ed3d8b64e8bcdc7e1c3a14e3e51c8ed
Bilal Sahli <Bilalsahli1999@gmail.com>
2022-09-20 09:34:32 +0200

    console web app with a httpClient  that sends a httpRequest to a github api and retrieve data from the api server in json format.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+namespace IntrusionDetectionSystem
+{
+    class Program
+    {
+        private static readonly HttpClient client = new HttpClient(); 
+        private static async Task ProcessRepositories()
+        {
+            client.DefaultRequestHeaders.Accept.Clear(); 
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json")); 
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter"); 
+
+            var stringTask = client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
+
+            var msg = await stringTask; 
+
+            Console.Write(msg); 
+        }
+        static async Task Main(String[] args)
+        {
+            await ProcessRepositories();
+        }
+
+    }
+
+}
+
+
+
+
