cfce2a37d9da2a25e4870a6a9af4cd2751108dae
Bilal Sahli <Bilalsahli1999@gmail.com>
2022-09-27 12:52:03 +0200

    classJsonDeserialiser

--- Program.cs
+++ Program.cs
@@ -3,23 +3,32 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text.Json;
 using static IntrusionDetectionSystem.PrometheusParser; 
-
+using  Microsoft.Extensions.Logging; 
 
 namespace IntrusionDetectionSystem
 {
     class Program
     {
         private static readonly HttpClient client = new HttpClient(); 
+        private readonly ILogger <Program> _log; 
+
+        public Program (ILogger<Program> log) 
+        {
+            _log = log; 
+        }
         private static async Task ProcessRepositories()
         {
             client.DefaultRequestHeaders.Accept.Clear(); 
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json")); 
             client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter"); 
+            string promQuery = "hosts_src_dst"; 
+            string url = "http://127.0.0.1:5500/sampleJson.html"; 
 
-            var stringTask = client.GetStringAsync("http://127.0.0.1:4000/metrics");
+            //var stringTask = client.GetStringAsync("http://10.9.10.14:9090/api/v1/query?query=" + promQuery);
+            var stringTask = client.GetStringAsync(url);
            // var metrics = await JsonSerializer.DeserializeAsync<List<Metrics>>(await streamTask);
             var msg = await stringTask; 
-           // Console.Write(msg); 
+            Console.Write(msg); 
             Parse(await stringTask); 
           
         }
