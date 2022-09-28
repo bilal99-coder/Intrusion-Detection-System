33f7873a68cd8210836ea8377d777d6c45b2ccdf
Bilal Sahli <Bilalsahli1999@gmail.com>
2022-09-28 12:29:01 +0200

    Add prometheus architecture screenshot

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using System.Net.Http.Headers;
 using System.Text.Json;
 using static IntrusionDetectionSystem.PrometheusParser; 
 using  Microsoft.Extensions.Logging; 
-
+using Model; 
 namespace IntrusionDetectionSystem
 {
     class Program
@@ -22,14 +22,21 @@ namespace IntrusionDetectionSystem
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json")); 
             client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter"); 
             string promQuery = "hosts_src_dst"; 
-            string url = "http://127.0.0.1:5500/sampleJson.html"; 
+            string url = "http://10.9.10.14:9090/api/v1/query?query=hosts_src_dst"; 
 
             //var stringTask = client.GetStringAsync("http://10.9.10.14:9090/api/v1/query?query=" + promQuery);
-            var stringTask = client.GetStringAsync(url);
+           // var stringTask = client.GetStringAsync(url);
+           var streamTask = client.GetStreamAsync(url);
+           Root myDeserializedClass = await JsonSerializer.DeserializeAsync<Root>(await streamTask); 
            // var metrics = await JsonSerializer.DeserializeAsync<List<Metrics>>(await streamTask);
-            var msg = await stringTask; 
-            Console.Write(msg); 
-            Parse(await stringTask); 
+           // var msg = await stringTask; 
+           // Console.Write(msg); 
+           // Parse(await stringTask); 
+
+           foreach (var result in myDeserializedClass.data.result) {
+               // Console.WriteLine("Connection going from: " + result.metric.a_srcAddr + " to: " + result.metric.b_dstAddr +  " at " + result.metric.c_dstPort.Split('/')[0] + " port number: " + result.metric.c_dstPort.Split('/')[1]); 
+               whileListe.add(result.metric.a_srcAddr);
+           }
           
         }
         static async Task Main(String[] args)
